ca90b04bd4d97e3b13de2c1e0064aa2ec40c9c6e
kosal.thoeurn <kosal.thoeurn@wingmoney.com>
2020-10-12 13:07:31 +0700

    initail cm

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-// import About from './Components/About';
+import About from './Components/About';
 import Resume from './Components/Resume';
-// import Contact from './Components/Contact';
-// import Testimonials from './Components/Testimonials';
-// import Portfolio from './Components/Portfolio';
+import Contact from './Components/Contact';
+import Testimonials from './Components/Testimonials';
+import Portfolio from './Components/Portfolio';
 
 class App extends Component {
 
@@ -47,11 +47,11 @@ class App extends Component {
     return (
       <div className="App">
         <Header data={this.state.resumeData.main}/>
-        {/* <About data={this.state.resumeData.main}/> */}
+        <About data={this.state.resumeData.main}/>
         <Resume data={this.state.resumeData.resume}/>
-        {/* <Portfolio data={this.state.resumeData.portfolio}/> */}
-        {/* <Testimonials data={this.state.resumeData.testimonials}/> */}
-        {/* <Contact data={this.state.resumeData.main}/> */}
+        <Portfolio data={this.state.resumeData.portfolio}/>
+        <Testimonials data={this.state.resumeData.testimonials}/>
+        <Contact data={this.state.resumeData.main}/>
         <Footer data={this.state.resumeData.main}/>
       </div>
     );
