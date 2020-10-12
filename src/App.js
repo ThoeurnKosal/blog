5f0766d382be65bfda8b2521ab3112a0160d1d82
kosal.thoeurn <kosal.thoeurn@wingmoney.com>
2020-10-12 12:00:04 +0700

    initail commit

--- src/App.js
+++ src/App.js
@@ -4,11 +4,11 @@ import $ from 'jquery';
 import './App.css';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-// import About from './Components/About';
+import About from './Components/About';
 import Resume from './Components/Resume';
-// import Contact from './Components/Contact';
-// import Testimonials from './Components/Testimonials';
-// import Portfolio from './Components/Portfolio';
+import Contact from './Components/Contact';
+import Testimonials from './Components/Testimonials';
+import Portfolio from './Components/Portfolio';
 
 class App extends Component {
 
